1b3573699ac9cbff99fe917793a02a11e4d14f57
A-Alex-ander <Dubrov.work@gmail.com>
2024-02-03 17:00:34 +0700

    video-12

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbrjYmArEmG4k9u9QtS3YbM1wL8dc_2hscUg&usqp=CAU"
+                alt="nike"/>
+        </header>
+
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div><img
+                src="https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"
+                alt=""/>
+            </div>
+            <div>
+                ava + descriptions
+            </div>
+            <div>
+                My posts
+                <div>
+                    New posts
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Profile;
