3eb27538d4ff4b040ca25917ceadc5fd557278a6
A-Alex-ander <Dubrov.work@gmail.com>
2024-02-04 21:48:55 +0700

    video-19-Routing,route,browser-router

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,25 @@ import Header from "./components/header/Header";
 import NavBar from "./components/navBar/NavBar";
 import Profile from "./components/profile/Profile";
 import Dialogs from "./components/dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <NavBar/>
-            <div className='app-wrapper-content'>
-                <Dialogs/>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <NavBar/>
+                <div className='app-wrapper-content'>
+                    <Route path={"/dialogs"} component={Dialogs}/>
+                    <Route path={"/profile"} component={Profile}/>
+                    <Route path={"/news"} component={Profile}/>
+                    <Route path={"/music"} component={Profile}/>
+                    <Route path={"/settings"} component={Profile}/>
+                </div>
             </div>
-            {/*<Profile/>*/}
+        </BrowserRouter>
 
-        </div>
 
     );
 }
