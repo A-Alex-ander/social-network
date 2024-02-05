150312fcfaac90a6cdb84b9e003d6017d0ad3f98
A-Alex-ander <Dubrov.work@gmail.com>
2024-02-05 16:13:55 +0700

    video-21-dialog page layout

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -2,8 +2,19 @@ import React from "react";
 import s from './Dialogs.module.css'
 const Dialogs = ()=>{
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + " " + s.active}>Alexandr</div>
+                <div className={s.dialog}>Vladislav</div>
+                <div className={s.dialog}>Nikita</div>
+                <div className={s.dialog}>Efim</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you ?</div>
+                <div className={s.message}>Good</div>
+            </div>
+
         </div>
     )
 }
